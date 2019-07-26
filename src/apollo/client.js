3eccd01380992681da30b5ddbb5ef548bc0f4116
Adriel Tosi <adrielstosi@gmail.com>
2019-07-26 11:31:40 +0200

    small change to client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -5,6 +5,6 @@ export const client = new ApolloClient({
   uri: "https://api-euwest.graphcms.com/v1/cjy49fo1n1m2801clm6kln3l9/master",
   fetch,
   headers: {
-    Authorization: `Bearer ${process.env.GATSBY_AUTH_TOKEN_MUTATION}`,
+    authorization: `Bearer ${process.env.GATSBY_AUTH_TOKEN_MUTATION}`,
   },
 })
