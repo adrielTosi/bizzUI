6a12f3492b7b66516372de0c4eafbad29eecc37f
Adriel Tosi <adrielstosi@gmail.com>
2019-08-07 19:23:20 +0200

    small changes to SEO

--- src/pages/BizzUI.js
+++ src/pages/BizzUI.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import SEO from "../components/common/seo"
 import BQuizzContainer from "../components/bizzUI/BQuizzContainer"
 import Layout from "layouts/BizzUILayout"
 
@@ -9,6 +10,10 @@ import "styles/index.scss"
 const BizzUI = ({ location }) => {
   return (
     <Layout>
+      <SEO
+        title="Bizz UI"
+        description={`A quizz to better understand how users feel about UI pieces.`}
+      />
       <BQuizzContainer location={location} />
     </Layout>
   )
