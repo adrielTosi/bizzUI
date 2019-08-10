73936d146afbb9f298ec5c8ed56e3d1a4503b512
Adriel Tosi <adrielstosi@gmail.com>
2019-08-10 16:00:06 +0200

    Added icons to stack items

--- src/components/portfolio/Switcher/Switcher.js
+++ src/components/portfolio/Switcher/Switcher.js
@@ -34,7 +34,7 @@ const Switcher = () => {
           action={context.setTab}
         />
       ))}
-      <div className="col-12 col-md-10 mt-4 mb-5">
+      <div className="col-12 col-md-10 mt-4 mb-5 justify-content-center">
         <Display tabs={tabs} currentTab={context.pState.currentTab} />
       </div>
     </SwitcherContainer>
