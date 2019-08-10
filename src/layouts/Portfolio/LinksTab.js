ecb30e60aed4d674595589eb98d7967f095ec196
Adriel Tosi <adrielstosi@gmail.com>
2019-08-10 16:30:55 +0200

    Added additional tab for Bizz UI route

--- src/layouts/Portfolio/LinksTab.js
+++ src/layouts/Portfolio/LinksTab.js
@@ -1,8 +1,10 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import style from "./portfolio.module.scss"
 import linkedin from "images/Portfolio/icons/linkedin.svg"
 import github from "images/Portfolio/icons/github.svg"
+
+import logo from "images/BizzUI/logo-color-no-name.png"
 
 const LinksTab = () => {
   return (
@@ -22,6 +24,12 @@ const LinksTab = () => {
         >
           <img src={github} alt="github" />
         </a>
+      </div>
+      <div className={style.bizzTab}>
+        <Link to="/BizzUI">
+          <img src={logo} alt="linkedin" />
+        </Link>
+        <div>Vote!</div>
       </div>
     </div>
   )
